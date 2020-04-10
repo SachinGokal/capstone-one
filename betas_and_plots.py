3116a0a76f7d90aec243d72bb14e637f8e151e47
Sachin Gokal <sachin.gokal@gmail.com>
2020-04-09 23:30:38 -0400

    updated plots

--- betas_and_plots.py
+++ betas_and_plots.py
@@ -85,6 +85,16 @@ class SectorBetasAndPlots:
     def create_csv_for_data_frame(df, title):
       return df.to_csv(f'{title}')
 
+    def plot_histogram_of_betas(df):
+      fig, ax = plt.subplots(figsize=(12, 10))
+      ax.set_title('Histogram of Betas for each sector', fontsize=18)
+      ax.set_ylabel('frequence', fontsize=14)
+      ax.set_xlabel('beta value', fontsize=14)
+      plt.tight_layout()
+      df.rename(columns=IMPT_BETA_COLUMNS)[
+          IMPT_BETA_COLUMNS.values()].hist(sharey=True, ax=ax)
+      plt.savefig('Distribution of Betas over 5 year period.png')
+
     def plot_sector_betas_over_time(betas_df, title):
       fig, axs = plt.subplots(6, 2, sharey=True, figsize=(15, 35))
       plt.tight_layout()
@@ -108,11 +118,19 @@ class SectorBetasAndPlots:
       ax.set_title(title, fontsize=18)
       ax.set_ylabel('beta', fontsize=14)
       ax.set_xlabel('sector', fontsize=14)
-
-      df[['sector', 'recent_average_beta', 'historical_average_beta']].plot(kind='bar', x='sector', ax=ax)
-
+      new_df = df
+      new_df['difference'] = df['historical_average_beta'] - df['recent_average_beta']
+      new_df.sort_values(by='difference', ascending=True, inplace=True)
+      new_df[['sector', 'historical_average_beta', 'recent_average_beta']].plot(kind='bar', x='sector', ax=ax)
       plt.tight_layout()
       plt.savefig(title)
+
+    def plot_histogram_of_betas(df):
+      fig, ax = plt.subplots(figsize=(12, 8))
+      ax.set_title('Histogram of Betas for each sector', fontsize=18)
+      ax.set_ylabel('frequence', fontsize=14)
+      ax.set_xlabel('beta value', fontsize=14)
+      df[IMPT_BETA_COLUMNS.keys()].hist(ax=ax)
 
     def get_data_for_a_period(df, start_date, end_date):
       new_df = df.reset_index()
@@ -164,12 +182,13 @@ class SectorBetasAndPlots:
     def plot_correlation(df, title):
       fig, ax = plt.subplots(figsize=(12, 8))
       ax.set_title(title, fontsize=18)
-      ax.set_ylabel('correlation', fontsize=16)
-      ax.set_xlabel('sector', fontsize=16)
       plt.xticks(fontsize=12)
-      df[['sector', 'recent_corr', 'one_month_corr', 'three_month_corr', 'one_year_corr', 'five_year_corr']].plot(
-          kind='box', x='sector', ax=ax)
-
+      new_corr_df['recent_cat'] = 'recent'
+      sns.stripplot(ax=ax, x="category", y="correlation", data=df, jitter=0.05)
+      ax.set_ylabel('correlation', fontsize=16)
+      ax.set_xlabel('time period', fontsize=16)
+      plt.tight_layout()
+      plt.savefig(title)
       plt.tight_layout()
       plt.savefig(title)
 
